9c285500839b4246fe58ef079c7076245472f759
Cheng Liangyu <clyfish@gmail.com>
2023-06-01 21:30:30 +0800

    feat: update brotli build script

--- build_brotli.sh
+++ build_brotli.sh
@@ -2,8 +2,9 @@
 
 cd "$(dirname "$0")" || exit 1
 
-git clone https://github.com/google/brotli
-cd brotli || exit 1
+wget https://github.com/google/brotli/archive/refs/tags/v1.0.9.tar.gz -O brotli-1.0.9.tar.gz
+tar xf brotli-1.0.9.tar.gz
+cd brotli-1.0.9 || exit 1
 rm -r out
 mkdir out
 cd out
